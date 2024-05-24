3cfea795e66131e8c3904803a6d80cd0d0910828
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-05-24 16:54:50 +0200

    implements get neighbors method for grid class

--- Core/inc/grid.h
+++ Core/inc/grid.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <cstdint>
+#include <stdexcept>
+
 #include "cell.h"
 #include "obstacle_gen.h"
 
@@ -11,21 +13,42 @@ namespace GridGenerator {
 
     class Grid {
     public:
-        Grid(uint32_t size, float obstacleDensity, ObstacleGenerator& generator) : size(size), cells(size, std::vector<Cell>(size)), start(nullptr),
-                                              end(nullptr) {
+        Grid(uint32_t sizeX, uint32_t sizeY, float obstacleDensity, ObstacleGenerator &generator) :
+                sizeX(sizeX), sizeY(sizeY), cells(sizeX, std::vector<Cell>(sizeY)), start(nullptr), end(nullptr) {
             generator.generateObstacles(cells, obstacleDensity);
-            //TODO: set start and end cell in init
+            //TODO: set start and end cell in init or in generateObstacles
             init();
-            calculateCosts();
         }
 
-        inline Cell* getStart(){return start;}
-        inline Cell* getEnd(){return start;}
+        Cell& operator() (size_t row, size_t col) {
+            if(row >= sizeX || col >= sizeY)
+                throw std::out_of_range("Grid indices out of range");
+            return cells[row][col];
+        }
+
+        const Cell& operator() (size_t row, size_t col) const {
+            if(row >= sizeX || col >= sizeY)
+                throw std::out_of_range("Grid indices out of range");
+            return cells[row][col];
+        }
+
+
+        std::vector<std::reference_wrapper<Cell>> getNeighbors(size_t row, size_t col);
+
+
+
+
+
+        inline Cell *getStart() { return start; }
+
+        inline Cell *getEnd() { return start; }
+
 
     private:
         void init();
-        void calculateCosts();
-        uint32_t size;
+
+        uint32_t sizeX;
+        uint32_t sizeY;
         std::vector<std::vector<Cell>> cells;
         Cell *start;
         Cell *end;
--- Core/src/grid.cpp
+++ Core/src/grid.cpp
@@ -1,12 +1,22 @@
 #include "../inc/grid.h"
 
-void GridGenerator::Grid::calculateCosts() {
-    for (auto& row : cells){
-        for(auto& cell: row){
-            if(cell.getState() != CellState::CELL_OBSTACLE){
-                cell.setGCost(calculateGCost(&cell));
-                cell.setHCost(calculateHCost(&cell));
+#include <functional>
+
+
+std::vector<std::reference_wrapper<GridGenerator::Cell>> GridGenerator::Grid::getNeighbors(size_t row, size_t col) {
+        std::vector<std::reference_wrapper<Cell>> neighbors;
+
+        std::vector<std::pair<int, int>> offsets = {
+                {-1, 0}, {1, 0}, {0, -1}, {0, 1},  // Top, Bottom, Left, Right
+                 {-1, -1}, {-1, 1}, {1, -1}, {1, 1}  // Diagonals
+        };
+
+        for (const auto& offset : offsets) {
+            int64_t neighborRow = row + offset.first;
+            int64_t neighborCol = col + offset.second;
+            if (neighborRow >= 0 && neighborRow < sizeX && neighborCol >= 0 && neighborCol < sizeY) {
+                neighbors.push_back(std::ref(cells[neighborRow][neighborCol]));
             }
         }
-    }
+        return neighbors;
 }
