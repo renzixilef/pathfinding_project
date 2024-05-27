0f37d939a4fd3c565a24c1e3c464be1acc5102ac
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-05-27 20:10:48 +0200

    implements qt5 cmake dependencies

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -1,8 +1,11 @@
 #include <cstdlib>
 #include <iostream>
+#include <QApplication>
+#include <QCommandLineParser>
+
 #include "../inc/grid.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout << "Hello world" << std::endl;
 
     return EXIT_SUCCESS;
