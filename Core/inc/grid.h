087b7035b2d2565c8d400e0a45937c705f400897
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-05-28 13:57:30 +0200

    safety changes to grid functionality

--- Core/inc/grid.h
+++ Core/inc/grid.h
@@ -10,7 +10,7 @@
 //TODO: documentation
 
 namespace GridGenerator {
-    struct GridCoordinates{
+    struct GridCoordinates {
         uint32_t x;
         uint32_t y;
     };
@@ -20,26 +20,31 @@ namespace GridGenerator {
     public:
         Grid(uint32_t sizeX, uint32_t sizeY, float obstacleDensity, ObstacleGenerator &generator);
 
-        Cell& operator() (GridCoordinates coords) {
-            if(coords.x >= sizeX || coords.y >= sizeY)
+        Cell &operator()(GridCoordinates coords) {
+            if (coords.x >= sizeX || coords.y >= sizeY)
                 throw std::out_of_range("Grid indices out of range");
             return cells[coords.x][coords.y];
         }
 
-        const Cell& operator() (GridCoordinates coords) const {
-            if(coords.x >= sizeX || coords.y >= sizeY)
+        const Cell &operator()(GridCoordinates coords) const {
+            if (coords.x >= sizeX || coords.y >= sizeY)
                 throw std::out_of_range("Grid indices out of range");
             return cells[coords.x][coords.y];
         }
 
 
-        std::vector<std::reference_wrapper<Cell>> getNeighbors(GridCoordinates coords);
+        std::vector<std::reference_wrapper<Cell>> getNeighborsCells(GridCoordinates coords);
 
-        inline Cell *getStartCell() {return startCell;}
-        inline Cell *getEndCell() {return endCell;}
+        [[nodiscard]] std::vector<GridCoordinates> getNeighborsCoordinates(GridCoordinates coords) const;
 
-        inline GridCoordinates getStartCoordinates(){return startCoordinates;}
-        inline GridCoordinates getEndCoordinates(){return endCoordinates;}
+
+        [[nodiscard]] inline Cell *getStartCell() const { return startCell; }
+
+        [[nodiscard]] inline Cell *getEndCell() const { return endCell; }
+
+        [[nodiscard]] inline GridCoordinates getStartCoordinates() const { return startCoordinates; }
+
+        [[nodiscard]] inline GridCoordinates getEndCoordinates() const { return endCoordinates; }
 
 
     private:
--- Core/src/grid.cpp
+++ Core/src/grid.cpp
@@ -3,10 +3,11 @@
 #include <functional>
 
 
-std::vector<std::reference_wrapper<GridGenerator::Cell>> GridGenerator::Grid::getNeighbors(GridCoordinates coords) {
+std::vector<std::reference_wrapper<GridGenerator::Cell>>
+GridGenerator::Grid::getNeighborsCells(GridCoordinates coords) {
     std::vector<std::reference_wrapper<Cell>> neighbors;
 
-    std::vector<std::pair<int, int>> offsets = {
+    std::vector<std::pair<uint8_t, uint8_t>> offsets = {
             {-1, 0},
             {1,  0},
             {0,  -1},
@@ -27,6 +28,32 @@ std::vector<std::reference_wrapper<GridGenerator::Cell>> GridGenerator::Grid::ge
     return neighbors;
 }
 
+std::vector<GridGenerator::GridCoordinates>
+GridGenerator::Grid::getNeighborsCoordinates(GridGenerator::GridCoordinates coords) const{
+    std::vector<GridCoordinates> neighbors;
+
+    std::vector<std::pair<uint8_t, uint8_t>> offsets = {
+            {-1, 0},
+            {1,  0},
+            {0,  -1},
+            {0,  1},  // Top, Bottom, Left, Right
+            {-1, -1},
+            {-1, 1},
+            {1,  -1},
+            {1,  1}  // Diagonals
+    };
+
+    for (const auto &offset: offsets) {
+        int64_t neighborRow = coords.x + offset.first;
+        int64_t neighborCol = coords.y + offset.second;
+        if (neighborRow >= 0 && neighborRow < sizeX && neighborCol >= 0 && neighborCol < sizeY) {
+            neighbors.push_back(
+                    GridCoordinates{static_cast<uint32_t>(neighborRow), static_cast<uint32_t>(neighborCol)});
+        }
+    }
+    return neighbors;
+}
+
 GridGenerator::Grid::Grid(uint32_t sizeX, uint32_t sizeY, float obstacleDensity, ObstacleGenerator &generator) :
         sizeX(sizeX),
         sizeY(sizeY),
@@ -39,3 +66,5 @@ GridGenerator::Grid::Grid(uint32_t sizeX, uint32_t sizeY, float obstacleDensity,
     //TODO: set start and end cell in init or in generateObstacles
     init();
 }
+
+
