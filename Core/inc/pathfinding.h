01a47ee8598a185f3f3d11b088fa52dd042d71b6
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-05-30 13:03:11 +0200

    implements pathfinderStrategy enum and static const map to displayable strings

--- Core/inc/pathfinding.h
+++ Core/inc/pathfinding.h
@@ -6,24 +6,36 @@
 
 namespace Pathfinder {
 
+    enum class PathfinderStrategy {
+        PATHFINDER_DIJKSTRA = 1,
+        PATHFINDER_A_STAR = 2,
+    };
+
+    static const std::map<PathfinderStrategy, std::string> pathfindingStrategyToDisplayableText =
+            {{PathfinderStrategy::PATHFINDER_DIJKSTRA, "Dijkstra's Algorithm"},
+             {PathfinderStrategy::PATHFINDER_A_STAR,   "A* Algorithm"}};
+
     class pathfindingParent {
     public:
-        explicit pathfindingParent(GridGenerator::Grid& grid) : grid(grid){}
+        explicit pathfindingParent(GridGenerator::Grid &grid) : grid(grid) {}
+
         virtual void markShortestPath() = 0;
-        virtual inline std::map<GridGenerator::Cell*, GridGenerator::Cell*> getShortestPath(){return shortestPath;};
+
+        virtual inline std::map<GridGenerator::Cell *, GridGenerator::Cell *>
+        getShortestPath() { return shortestPath; };
 
     protected:
-        std::map<GridGenerator::Cell*, GridGenerator::Cell*> shortestPath;
-        GridGenerator::Grid& grid;
+        std::map<GridGenerator::Cell *, GridGenerator::Cell *> shortestPath;
+        GridGenerator::Grid &grid;
 
     };
 
-    class DijkstraSolve : public pathfindingParent{
+    class DijkstraSolve : public pathfindingParent {
     public:
         void markShortestPath() override;
     };
 
-    class AStarSolve : public pathfindingParent{
+    class AStarSolve : public pathfindingParent {
     public:
         void markShortestPath() override;
     };
