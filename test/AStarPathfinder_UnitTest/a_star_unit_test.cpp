44d43c5ec7a38daccf3aafb87a44c0fc38b589f9
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-06-24 17:44:55 +0200

    implements a star unit test needs refactoring of cmakelists before it works

--- test/AStarPathfinder_UnitTest/a_star_unit_test.cpp
+++ test/AStarPathfinder_UnitTest/a_star_unit_test.cpp
@@ -2,8 +2,9 @@
 #include <fstream>
 
 #include "Pathfinder/pathfinding.h"
+#include "GridGenerator/grid.h"
 
-TEST(AStarPathfinder, UnitTest){
+TEST(Pathfinder, AStarUnitTest){
     auto finishedGrid = GridGenerator::Grid::deserialize("astar_testgrid.bin");
     auto gridToSolve = finishedGrid;
     gridToSolve.resetGrid();
@@ -11,5 +12,10 @@ TEST(AStarPathfinder, UnitTest){
     Pathfinder::AStarSolve solver{gridToSolve};
     solver.solveNoWait();
 
-
+    for(uint32_t x = 0; x<finishedGrid.getSizeX(); x++){
+        for(uint32_t y = 0; y<finishedGrid.getSizeY(); y++){
+            GridGenerator::GridCoordinate coord{x,y};
+            EXPECT_EQ(finishedGrid(coord).getState(), gridToSolve(coord).getState());
+        }
+    }
 }
