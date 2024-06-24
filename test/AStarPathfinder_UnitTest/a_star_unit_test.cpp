753ec6655a53491d76ba87951a1901d4762e1445
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-06-24 16:49:51 +0200

    includes gTest chain in all config files, begins writing of a* test

---
+++ test/AStarPathfinder_UnitTest/a_star_unit_test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <fstream>
+
+#include "Pathfinder/pathfinding.h"
+
+TEST(AStarPathfinder, UnitTest){
+    auto finishedGrid = GridGenerator::Grid::deserialize("astar_testgrid.bin");
+    auto gridToSolve = finishedGrid;
+    gridToSolve.resetGrid();
+
+    Pathfinder::AStarSolve solver{gridToSolve};
+
+
+
+}
