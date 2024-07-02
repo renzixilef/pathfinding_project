c77d3d687df98e0948fa2d609909e0eca93c2325
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-07-02 16:20:53 +0200

    implements option wrapper for command line options to create sets

---
+++ Core/Application/inc/command_line_parser.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QSet>
+#include <iterator>
+
+
+namespace Application {
+    class PathfindingCommandParser : public QCommandLineParser {
+
+    };
+
+}
+
--- Core/main.cpp
+++ Core/main.cpp
@@ -4,25 +4,38 @@
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include <QDebug>
+#include <memory>
 
 #include "gui_main_window.h"
 
 int main(int argc, char *argv[]) {
-    QApplication pathfindingApp(argc, argv);
+    std::unique_ptr<QCoreApplication> pathfindingApp = std::make_unique<QCoreApplication>(argc,argv);
+    QCoreApplication::setApplicationName("Pathfinding");
+    QCoreApplication::setApplicationVersion("1.0");
 
     QCommandLineParser pathfindingParser;
     pathfindingParser.setApplicationDescription(
             "Command-line and GUI Application for pathfinding algorithm benchmarking");
+    pathfindingParser.addHelpOption();
+    pathfindingParser.addVersionOption();
 
     QCommandLineOption guiOption(QStringList() << "g" << "gui", "Starts the application in GUI mode");
     pathfindingParser.addOption(guiOption);
 
-    //TODO: implement additional benchmarking command line options
-    //TODO: implement gui and other options mutually exclusive
+    QCommandLineOption headlessOption(QStringList() << "h" << "headless", "Starts the application in headless mode");
+    pathfindingParser.addOption(guiOption);
 
-    pathfindingParser.process(pathfindingApp);
+
+    pathfindingParser.process(*pathfindingApp);
+
     bool guiMode = pathfindingParser.isSet(guiOption);
+    bool headlessMode = pathfindingParser.isSet(headlessOption);
+
+
+
+
     if(guiMode){
+        pathfindingApp = std::make_unique<QApplication>(argc, argv);
         QMainWindow* mainWindow = new GUI::MainWindow();
         mainWindow->show();
     }else{
